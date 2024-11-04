6779abcff82082f3f315481d1442a5f5a1547d64
Kobe Palmenco <101782692+Kobe0207@users.noreply.github.com>
2024-11-04 13:54:21 +0800

    Add files via upload

--- Palmenco_Lab9.py
+++ Palmenco_Lab9.py
@@ -1,8 +1,18 @@
-rows = input("Enter the number of rows: ")
+#Enter the num of rows
+num_rows =int(input('Enter The number of rows: '))
 
-p = 1
-for i in range(int(rows)):
-  for j in range(i + 1):
-    print(p , end=" ")
-    p += 1
-  print()
+row_number = 1 
+triangle_row = ''
+
+
+for row in range(1, num_rows + 1): 
+
+    for column in range(row): 
+
+      triangle_row += str(row_number) + ' ' 
+
+      row_number += 1 
+
+    print(triangle_row) 
+
+    triangle_row = '' 
